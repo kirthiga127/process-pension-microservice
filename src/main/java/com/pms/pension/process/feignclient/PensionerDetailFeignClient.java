9dd31f4e5cd8a01e8eda7013139396891a81679d
kirthiga127 <81847005+kirthiga127@users.noreply.github.com>
2022-05-31 08:57:04 +0530

    Update PensionerDetailFeignClient.java

--- src/main/java/com/pms/pension/process/feignclient/PensionerDetailFeignClient.java
+++ src/main/java/com/pms/pension/process/feignclient/PensionerDetailFeignClient.java
@@ -12,7 +12,7 @@ import com.pms.pension.process.model.PensionerDetail;
 import io.swagger.annotations.ApiParam;
 
 //@FeignClient(name ="pensionerdetail-microservice",url = "http://localhost:8200/pensioner/api/final")
-@FeignClient(name ="pensionerdetail-microservice",url = "http://890387-pensioner-detail-lb-341589305.us-east-1.elb.amazonaws.com/pensioner/api/final")
+@FeignClient(name ="pensionerdetail-microservice",url = "http://890387-pensioner-detail-lb-1694207851.us-east-1.elb.amazonaws.com/pensioner/api/final")
 public interface PensionerDetailFeignClient {
 	
 	@GetMapping("/PensionerDetailByAadhaar/{aadharNumber}")
